e910cd3f241a87d43a499f8a8ecd29cc1c7be41a
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2021-03-15 18:35:48 +0100

    Serde as feature

--- confidential-identity/src/sign.rs
+++ confidential-identity/src/sign.rs
@@ -6,6 +6,7 @@ use curve25519_dalek::{
     ristretto::{CompressedRistretto, RistrettoPoint},
     scalar::Scalar,
 };
+#[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use sha3::{digest::FixedOutput, Digest, Sha3_512};
 use zeroize::Zeroize;
@@ -44,7 +45,8 @@ pub struct PublicKey {
 }
 
 /// Stores the Schnorr signature for verifying the wellformedness of scope_id.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[allow(non_snake_case)]
 pub struct Signature {
     pub(crate) R: CompressedRistretto,
